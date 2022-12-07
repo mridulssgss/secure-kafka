016340a4c609249d6547a2d8d5e47c82d44fb502
arkashisdas <arkashis.das2014@gmail.com>
2022-12-07 11:17:42 +0530

    changes to insecure_consumer

--- aes_256_eax_kafka_encryption/kafka_consumer_insecure.py
+++ aes_256_eax_kafka_encryption/kafka_consumer_insecure.py
@@ -2,6 +2,6 @@ import kafka
 cns = kafka.KafkaConsumer("python_tests", bootstrap_servers=['localhost:9092'], auto_offset_reset='latest')
 
 for msg in cns:
-    print(msg.value)
+    print(msg.timestamp,": ", str(msg.value))
 
 
